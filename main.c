2f7e2aaf38dc82dcf40c28d5972854d78182e95d
moccot <justother94@gmail.com>
2023-10-11 12:46:18 -0400

    Update: Add sim_purch.h

--- main.c
+++ main.c
@@ -1,67 +1,103 @@
 #include <stdio.h>
+#include "sim_purch.h"
 
+// O objetivo do programa é simular um sistema de super-mercado.
+
+// Código das ações para o menu.
 typedef enum {
-    PURCH_SIM,
-    SEARCH_DEBT,
-    FILL_STOCK,
-    SEARCH_STOCK,
-    PAY_DEBT,
-    FORGV_DEBT
-} action;
+    SIM_PURCH, // Simular compra.
+    SEARCH_DEBT, // Pesquisar débito de um cliente.
+    FILL_STOCK, // Reabastecer estoque.
+    SEARCH_STOCK, // Pesquisar estoque.
+    PAY_DEBT, // Pagar débito de um cliente.
+    FORGV_DEBT, // Perdoar débito de um cliente aleatório.
+    EXIT_ACTION // Sair.
+} action_code;
 
-int get_action();
+int main()
+{
+    double PRODUCTS_PRICE[NUM_PRODUCTS];
 
-void sim_purch();
-void search_debt();
-void fill_stock();
-void search_stock();
-void pay_debt();
-void forgv_debt();
+    int PRODUCTS_STOCK[NUM_PRODUCTS];
 
-int main() {
-    int action = get_action();
+    const double PAYMENT_DISCOUNT[4] = { // Desconto de cada forma de pagamento.
+        0.10, // Dinheiro.
+        0.05, // Cartão (débito ou crédito).
+        0.12, // Pix.
+        0 // Crediário.
+    };
 
-    switch (action) {
-        case PURCH_SIM:
-            sim_purch();
-            break;
-        case SEARCH_DEBT:
-            search_debt();
-            break;
-        case FILL_STOCK:
-            fill_stock();
-            break;
-        case SEARCH_STOCK:
-            search_stock();
-            break;
-        case PAY_DEBT:
-            pay_debt();
-            break;
-        case FORGV_DEBT:
-            forgv_debt();
-            break;
-        default:
-            printf("Ação inválida.");
+    // Definindo os preços dos produtos.
+    PRODUCTS_PRICE[0] = 0.50; // Pão.
+    PRODUCTS_PRICE[1] = 3.50; // Leite.
+    PRODUCTS_PRICE[2] = 5.00; // Café.
+    PRODUCTS_PRICE[3] = 10.00; // Arroz.
+    PRODUCTS_PRICE[4] = 8.00; // Feijão.
+    PRODUCTS_PRICE[5] = 3.00; // Açúcar.
+    PRODUCTS_PRICE[6] = 2.00; // Sal.
+    PRODUCTS_PRICE[7] = 5.00; // Óleo.
+    PRODUCTS_PRICE[8] = 3.00; // Farinha.
+    PRODUCTS_PRICE[9] = 30.00; // Carne.
+
+    // Definindo os estoques dos produtos.
+    PRODUCTS_STOCK[0] = 100; // Pão.
+    PRODUCTS_STOCK[1] = 100; // Leite.
+    PRODUCTS_STOCK[2] = 100; // Café.
+    PRODUCTS_STOCK[3] = 100; // Arroz.
+    PRODUCTS_STOCK[4] = 100; // Feijão.
+    PRODUCTS_STOCK[5] = 100; // Açúcar.
+    PRODUCTS_STOCK[6] = 100; // Sal.
+    PRODUCTS_STOCK[7] = 100; // Óleo.
+    PRODUCTS_STOCK[8] = 100; // Farinha.
+    PRODUCTS_STOCK[9] = 100; // Carne.
+
+    action_code action = 0; // Código da ação escolhida pelo usuário.
+
+    // Loop principal do programa.
+    while (action != EXIT_ACTION) {
+        // Menu de opções.
+        printf("Escolha uma opção:\n");
+        printf("[%d] - Simular compra.\n", SIM_PURCH);
+        printf("[%d] - Pesquisar débito de um cliente.\n", SEARCH_DEBT);
+        printf("[%d] - Reabastecer estoque.\n", FILL_STOCK);
+        printf("[%d] - Pesquisar estoque.\n", SEARCH_STOCK);
+        printf("[%d] - Pagar débito de um cliente aleatório.\n", PAY_DEBT);
+        printf("[%d] - Perdoar débito de um cliente aleatório.\n", FORGV_DEBT);
+        printf("[%d] - Sair.\n", EXIT_ACTION);
+
+        printf("Selecione uma opção: ");
+        scanf("%d", &action);
+
+        // Executa a ação escolhida pelo usuário.
+        switch (action) {
+            case SIM_PURCH:
+                sim_purch((const double *) &PRODUCTS_PRICE, (int *) &PRODUCTS_STOCK,
+                          (const double *) &PAYMENT_DISCOUNT);
+                break;
+            case SEARCH_DEBT:
+                printf("Pesquisar débito de um cliente.\n");
+                break;
+            case FILL_STOCK:
+                printf("Reabastecer estoque.\n");
+                break;
+            case SEARCH_STOCK:
+                printf("Pesquisar estoque.\n");
+                break;
+            case PAY_DEBT:
+                printf("Pagar débito de um cliente.\n");
+                break;
+            case FORGV_DEBT:
+                printf("Perdoar débito de um cliente aleatório.\n");
+                break;
+            case EXIT_ACTION:
+                printf("Sair.\n");
+                return 0;
+            default:
+                printf("Opção inválida.\n");
+                break;
+        }
     }
 
     printf("Fim do programa.\n");
     return 0;
-}
-
-int get_action()  {
-    int action;
-
-    printf("MENU:\n");
-    printf("[%d] Simular compra\n", PURCH_SIM);
-    printf("[%d] Consultar débito\n", SEARCH_DEBT);
-    printf("[%d] Preencher estoque\n", FILL_STOCK);
-    printf("[%d] Consultar estoque\n", SEARCH_STOCK);
-    printf("[%d] Pagar débito\n", PAY_DEBT);
-    printf("[%d] Perdoar dívida.\n", FORGV_DEBT);
-
-    printf("Ação: ");
-
-    scanf("%d", &action);
-
-    return action;
 }
---
+++ sim_purch.h
@@ -0,0 +1,183 @@
+//
+// Created by Tarcísio on 11/10/2023.
+//
+
+#ifndef TRABALHO_FINAL_ALG1_SIM_PURCH_H
+#define TRABALHO_FINAL_ALG1_SIM_PURCH_H
+#include <stdio.h>
+
+#endif //TRABALHO_FINAL_ALG1_SIM_PURCH_H
+
+// Código dos produtos.
+typedef enum {
+    BREAD, // Pão.
+    MILK, // Leite.
+    COFFEE, // Café.
+    RICE, // Arroz.
+    BEANS, // Feijão.
+    SUGAR, // Açúcar.
+    SALT, // Sal.
+    OIL, // Óleo.
+    FLOUR, // Farinha.
+    MEAT, // Carne.
+    EXIT_PRODUCT_SEL
+} product_code;
+
+// Código das formas de pagamento.
+typedef enum {
+    CASH, // Dinheiro.
+    CARD, // Cartão (débito ou crédito).
+    PIX, // Pix.
+    CREDIARY, // Crediário.
+    EXIT_PAYMENT_SEL
+} payment_code;
+
+const int NUM_PRODUCTS = 10; // Número de produtos.
+
+void sim_purch(const double *PRODUCTS_PRICE, int* PRODUCTS_STOCK, const double* PAYMENT_DISCOUNT)
+{
+    printf("Simulando compra.\n");
+
+    int
+            purchases[NUM_PRODUCTS], // Armazena as compras para subtrair do estoque.
+    selection_i = 0,
+            max_selection = 100;
+    double total_price = 0, discount = 0; // Preço total da compra.
+    payment_code payment_code = 0; // Código da forma de pagamento escolhida pelo usuário.
+    product_code prod_code = 0; // Código do produto escolhido pelo usuário.
+
+    purchases[0] = 0; // Pão.
+    purchases[1] = 0; // Leite.
+    purchases[2] = 0; // Café.
+    purchases[3] = 0; // Arroz.
+    purchases[4] = 0; // Feijão.
+    purchases[5] = 0; // Açúcar.
+    purchases[6] = 0; // Sal.
+    purchases[7] = 0; // Óleo.
+    purchases[8] = 0; // Farinha.
+    purchases[9] = 0; // Carne.
+
+    // Loop da seleção de produtos.
+    while (selection_i < max_selection) {
+        int quantity = 0; // Quantidade do produto escolhido pelo usuário.
+        double product_discount = 0; // Desconto do produto escolhido pelo usuário em função da quantidade.
+        product_code temp_prod_code = 0; // Código do produto escolhido pelo usuário.
+
+        // Menu de opções.
+        printf("Escolha um produto:\n");
+        printf("[%d] - Pão.\n", BREAD);
+        printf("[%d] - Leite.\n", MILK);
+        printf("[%d] - Café.\n", COFFEE);
+        printf("[%d] - Arroz.\n", RICE);
+        printf("[%d] - Feijão.\n", BEANS);
+        printf("[%d] - Açúcar.\n", SUGAR);
+        printf("[%d] - Sal.\n", SALT);
+        printf("[%d] - Óleo.\n", OIL);
+        printf("[%d] - Farinha.\n", FLOUR);
+        printf("[%d] - Carne.\n", MEAT);
+        printf("[%d] - Finalizar compra.\n", EXIT_PRODUCT_SEL);
+
+        printf("Selecione uma opção: ");
+        scanf("%d", &temp_prod_code);
+
+        if (temp_prod_code == EXIT_PRODUCT_SEL) break;
+        else prod_code = temp_prod_code;
+
+        printf("Quantidade: ");
+        scanf("%d", &quantity);
+
+        // Verifica se a quantidade é válida.
+        if (quantity <= 0) {
+            printf("Quantidade inválida.\n");
+            continue;
+        }
+
+        // Verifica se há quantidade o suficiente no estoque.
+        if (PRODUCTS_STOCK[temp_prod_code] < quantity || PRODUCTS_STOCK[temp_prod_code] < purchases[temp_prod_code] + quantity) {
+            printf("Quantidade indisponível.\n");
+            continue;
+        }
+
+        // Checa se o código do produto é válido e atribui o desconto com acordo com a quantidade.
+        switch (temp_prod_code) {
+            case BREAD:
+                if (quantity >= 10) product_discount = 0.10;
+                break;
+            case MILK:
+                if (quantity >= 5) product_discount = 0.05;
+                break;
+            case COFFEE:
+                if (quantity >= 3) product_discount = 0.03;
+                break;
+            case RICE:
+            case BEANS:
+            case SUGAR:
+            case SALT:
+            case OIL:
+            case FLOUR:
+            case MEAT:
+                if (quantity >= 2) product_discount = 0.02;
+                break;
+            default:
+                printf("Opção inválida.\n");
+                break;
+        }
+
+        purchases[temp_prod_code] += quantity;
+        total_price += (PRODUCTS_PRICE[temp_prod_code] * quantity) * (1 - product_discount);
+
+        selection_i++;
+    }
+
+    if (total_price != 0) {
+        printf("Total: R$%.2f\n", total_price);
+
+        // Seleção da forma de pagamento.
+        printf("Escolha uma forma de pagamento:\n");
+        printf("[%d] - Dinheiro.\n", CASH);
+        printf("[%d] - Cartão (débito ou crédito).\n", CARD);
+        printf("[%d] - Pix.\n", PIX);
+        printf("[%d] - Crediário.\n", CREDIARY);
+        printf("[%d] - Cancelar compra.\n", EXIT_PAYMENT_SEL);
+
+        printf("Selecione uma opção: ");
+        scanf("%d", &payment_code);
+
+        // Verifica se a forma de pagamento é válida e aplica o  desconto de acordo.
+        switch (payment_code) {
+            case CASH:
+                discount = PAYMENT_DISCOUNT[CASH];
+                break;
+            case CARD:
+                discount = PAYMENT_DISCOUNT[CARD];
+                break;
+            case PIX:
+                discount = PAYMENT_DISCOUNT[PIX];
+                break;
+            case CREDIARY:
+                discount = PAYMENT_DISCOUNT[CREDIARY];
+                break;
+            case EXIT_PAYMENT_SEL:
+                printf("Compra cancelada.\n");
+                break;
+            default:
+                printf("Opção inválida.\n");
+                break;
+        }
+
+        if (payment_code != EXIT_PAYMENT_SEL) {
+            total_price *= (1 - discount);
+
+            printf("Total: R$%.2f\n", total_price);
+
+            // Subtrai itens do estoque.
+            PRODUCTS_STOCK[prod_code] -= purchases[prod_code];
+
+            getchar();
+
+            printf("Compra finalizada.\n");
+        } else printf("Compra cancelada.\n");
+    }
+
+    else printf("Compra cancelada.\n");
+}
